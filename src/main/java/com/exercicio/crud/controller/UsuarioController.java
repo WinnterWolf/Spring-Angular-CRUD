8905b0b68cc20a6f7169c8d2267bf2400550a7c7
WinnterWolf <patrick.winter@outlook.com.br>
2020-11-23 17:30:27 -0400

    added CrossOrigin to angular default host

--- src/main/java/com/exercicio/crud/controller/UsuarioController.java
+++ src/main/java/com/exercicio/crud/controller/UsuarioController.java
@@ -8,12 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@RestController @CrossOrigin(origins = "http://localhost:8080")
+@RestController @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1")
 public class UsuarioController {
 
